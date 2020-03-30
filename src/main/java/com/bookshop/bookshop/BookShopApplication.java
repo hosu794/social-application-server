a96ef19b7d5094bd81256dbfbfee5662de4714e5
gregory <grzesszczesny14@gmail.com>
2020-03-30 12:48:59 +0200

    Remove database from the project

--- src/main/java/com/bookshop/bookshop/BookShopApplication.java
+++ src/main/java/com/bookshop/bookshop/BookShopApplication.java
@@ -3,10 +3,8 @@ package com.bookshop.bookshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
+
 
 
 @SpringBootApplication
--- src/main/java/com/bookshop/bookshop/model/RoleName.java
+++
@@ -1,6 +0,0 @@
-package com.bookshop.bookshop.model;
-
-public enum RoleName {
-    ROLE_NAME,
-    ROLE_ADMIN
-}
