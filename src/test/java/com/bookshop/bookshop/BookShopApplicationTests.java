6cd2b7abb6ea0b0dea7b8adac020b8759337126f
gregory <grzesszczesny14@gmail.com>
2020-03-30 12:09:18 +0200

    Fix the next bugs

--- src/test/java/com/bookshop/bookshop/BookShopApplicationTests.java
+++ src/test/java/com/bookshop/bookshop/BookShopApplicationTests.java
@@ -1,19 +1,13 @@
 package com.bookshop.bookshop;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
 class BookShopApplicationTests {
 
 	@Test
 	void contextLoads() {
-        assertTrue(true);
-    }
+	}
 
 }
