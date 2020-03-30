f1b01c329ca183b38184b2d4413c3e5e5e3cc750
gregory <grzesszczesny14@gmail.com>
2020-03-30 11:11:08 +0200

    Setup all

--- src/main/java/com/bookshop/bookshop/BookShopApplication.java
+++ src/main/java/com/bookshop/bookshop/BookShopApplication.java
@@ -3,6 +3,7 @@ package com.bookshop.bookshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -14,11 +15,6 @@ import java.util.TimeZone;
 })
 public class BookShopApplication {
 
-
-	@PostConstruct
-	void init() {
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-	}
 
 
 	public static void main(String[] args) {
